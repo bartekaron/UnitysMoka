261655b6e98b02c0e037468051d0a32cbd28cb4e
vargajanos <janosvarga706@gmail.com>
2023-05-31 09:27:55 +0200

    Kamera fix

---
+++ Assets/Propeller.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Propeller : MonoBehaviour
+{
+    private float propeller;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        propeller = Input.GetAxis("Vertical");
+
+        transform.Rotate(Vector3.forward * 40);
+    }
+}
